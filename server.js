793e5882023601e21f5addfa20aebffeb3119f4e
Svyatix <actionrus72@mail.ru>
2020-12-14 19:34:20 +0500

    add modules to mongo use

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3001;
 
 const app = new express();
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://admin:'+process.env.dbAdmin+'@cluster0.dkanu.mongodb.net/users', {
+            useNewUrlParser: true,
+            useFindAndModify:false,
+            useUnifiedTopology: true
+        });
+        app.listen(PORT, () => {
+            console.log('Server has been started...');
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+app.use('/', require('./routes/users'));
+
+start();
