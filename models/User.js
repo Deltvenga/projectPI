020fe10df39d618b04c91c534033428f02a72a8f
Svyatix <actionrus72@mail.ru>
2021-01-09 18:00:11 +0500

    user model

--- models/User.js
+++ models/User.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     name: {
         type: String
+    },
+    password: {
+        type: String
+    },
+    icon: {
+        type: String
+    },
+    marks: {
+        type: Array
     }
 });
 module.exports = mongoose.model('user', UserSchema);
